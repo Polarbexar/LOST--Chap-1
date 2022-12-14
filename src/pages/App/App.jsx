b3972514a153750d0d91f704dbc76e3216f5be37
Polarbexar <jcameron.britton@gmail.com>
2022-12-14 12:31:47 -0700

    Ready Infrastructure

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,9 @@
-import './App.css';
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from "../AuthPage/AuthPage";
-import NewOrderPage from "../NewOrderPage/NewOrderPage";
-import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 import NavBar from "../../components/NavBar/NavBar";
+import './App.css';
 
 
 
@@ -20,8 +18,8 @@ export default function App() {
         <NavBar user={user} setUser={setUser}/>
         <Routes>
           {/* Route components in here */}
-          <Route path="/orders/new" element={<NewOrderPage />} />
-          <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="" element="" />
+          <Route path="" element="" />
         </Routes>
       </>
       :
